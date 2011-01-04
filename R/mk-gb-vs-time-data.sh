74e9a32037c98799618569bd45ccbe98510ee1d4
Todd Hepler <thepler@genome.wustl.edu>
2011-01-04 16:53:01 -0600

    some data for an R graph that idas worked on

---
+++ R/mk-gb-vs-time-data.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+perl -e 'print "day\tgb\n"' > gb-vs-time.txt
+
+#    select x.mydate, sum( x.bases ), sum( x.bases ) / 1000000000 From (
+sqlrun "
+
+    select x.mydate, sum( x.bases ) / 1000000000 From (
+        select to_char(pse.date_completed, 'yyyy-mm-dd') mydate, ri.kilobases_read bases
+        from index_illumina ii
+        join read_illumina ri on ri.ii_seq_id = ii.seq_id
+        join process_step_executions@oltp pse
+        on pse.pse_id = ii.creation_event_id
+        and pse.date_completed is not null
+        and pse.psesta_pse_status in ('inprogress', 'completed')
+    ) x
+    group by x.mydate
+    order by x.mydate
+
+" --instance warehouse --parse >> gb-vs-time.txt;
+
+#        (select pse.* from process_Step_executions pse where pse.ps_ps_id = 3601) gls
+#    select o.*, ii.seq_id from
+#    (
+#        select gls.pse_id, gls.date_completed, sp.seq_id from sequence_pse sp join
+#        (select pse.* from process_Step_executions pse join process_steps ps on ps.ps_id = pse.ps_ps_id and ps.pro_process_to = 'generate lane summary') gls
+#        on gls.pse_id = sp.pse_id
+#    ) o
+#    join index_illumina@dw ii
+#    on ii.seq_id = o.seq_id
