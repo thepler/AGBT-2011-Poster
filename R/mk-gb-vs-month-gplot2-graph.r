e310d020084051c0047dc18fd351a3ee6251629b
Todd Hepler <thepler@genome.wustl.edu>
2011-01-18 12:03:41 -0600

    tweaks from idas++

--- R/mk-gb-vs-month-gplot2-graph.r
+++ R/mk-gb-vs-month-gplot2-graph.r
@@ -23,7 +23,24 @@ svg(
 )
 #pdf(file="month.pdf")
 cat(paste("Plotting", "\n"))
-ggplot(data, aes(month, gb)) + geom_line()
+
+# Line Chart
+#ggplot(data, aes(month, gb)) + geom_line()
+
+# Scatter Plot with a Trend Line
+#qplot(month, gb, 
+#        data = data, 
+#        geom = c("point", "smooth"), 
+#        span = 0.5, # the "wiggle" of the trend line 0 no wiggle 1 most wiggle
+#        se = FALSE,
+#        main = "Illumina Bases Count"
+#)
+
+# Scatter Plot (as Bars)
+ggplot(data, aes(month, gb)) + geom_bar(stat="identity") + opts(title="Illumina Bases Count")
+#ggplot(data, aes(as.character(month), gb)) + geom_bar() # Experiment DO NOT USE
+
+# Regular "R Plotting"
 #plot(data)
 
 # turn off the SVG printing device
