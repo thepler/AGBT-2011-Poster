4d7c30e146c2f971797be6a44b7df7c586f67f57
Todd Hepler <thepler@genome.wustl.edu>
2012-07-16 14:35:54 -0500

    cleanup

--- R/mk-bases-vs-day-data.sh
+++ R/mk-bases-vs-day-data.sh
@@ -18,19 +18,9 @@ sqlrun "
                 (bi.index_sequence is null and ii.index_sequence is null)
             )
             and bi.date_completed is not null
---        where ii.flow_cell_id = '615HF'
     ) x
     group by x.mydate
     order by x.mydate
 
 " --instance warehouse --parse >> bases-vs-day.txt;
 
-#        (select pse.* from process_Step_executions pse where pse.ps_ps_id = 3601) gls
-#    select o.*, ii.seq_id from
-#    (
-#        select gls.pse_id, gls.date_completed, sp.seq_id from sequence_pse sp join
-#        (select pse.* from process_Step_executions pse join process_steps ps on ps.ps_id = pse.ps_ps_id and ps.pro_process_to = 'generate lane summary') gls
-#        on gls.pse_id = sp.pse_id
-#    ) o
-#    join index_illumina@dw ii
-#    on ii.seq_id = o.seq_id
