7b8c52c37041afc3a5067e70bec630d2dcbf08c1
Todd Hepler <thepler@genome.wustl.edu>
2012-07-16 14:27:42 -0500

    minor tweak. more data

--- R/mk-gb-vs-day-data.sh
+++ R/mk-gb-vs-day-data.sh
@@ -6,7 +6,7 @@ perl -e 'print "day\tgb\n"' > gb-vs-day.txt
 sqlrun "
 
     select x.mydate, sum( x.bases ) / 1000000000 From (
-        select to_char(pse.date_completed, 'yyyy-mm-dd') mydate, ri.kilobases_read bases
+        select to_char(pse.date_completed, 'yyyy-mm-dd') mydate, (ri.read_length * ii.filt_clusters) bases
         from index_illumina ii
         join read_illumina ri on ri.ii_seq_id = ii.seq_id
         join process_step_executions@oltp pse
